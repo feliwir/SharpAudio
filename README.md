55f10c2657bf78da69c750c48d624a70e473abd0
feliwir <stephan.vedder@gmail.com>
2018-10-29 21:57:50 +0100

    Add automatic versioning

--- README.md
+++ README.md
@@ -5,14 +5,45 @@ Supported backends:
 - XAudio2
 - OpenAL
 
-# Structure
-
-SharpAudio provides a low-level interface that wraps audio sources & buffers. A high level interface that can load and play sound files is provided in the SharpAudio.Util package. The following sound formats are supported at the moment:
-- `.wav` (PCM & ADPCM)
-- `.mp3` 
-
 
 # Build status
 
 [![Build status](https://ci.appveyor.com/api/projects/status/c3e98wk0mwcsje00?svg=true)](https://ci.appveyor.com/project/feliwir/sharpaudio)
 [![MyGet Badge](https://buildstats.info/myget/feliwir/SharpAudio)](https://www.myget.org/feed/feliwir/package/nuget/SharpAudio)
+
+# Example
+
+SharpAudio provides a low-level interface that wraps audio sources & buffers:
+```csharp
+  var engine = AudioEngine.CreateDefault();
+  var buffer = engine.CreateBuffer();
+  var source = engine.CreateSource();
+
+  AudioFormat format;
+  format.BitsPerSample = 16;
+  format.Channels = 1;
+  format.SampleRate = 44100;
+  float freq = 440.0f;
+  var size = format.SampleRate;
+  var samples = new short[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    samples[i] = (short)(32760 * Math.Sin((2 * Math.PI * freq) / size * i));
+  }
+
+  buffer.BufferData(samples, format);
+
+  source.QueryBuffer(buffer);
+
+  source.Play();
+```
+
+A high level interface that can load and play sound files is provided in the SharpAudio.Util package:
+```csharp
+```
+
+The following sound formats are supported at the moment:
+- `.wav` (PCM & ADPCM)
+- `.mp3` 
+- `.ogg` (WIP: Vorbis & Opus)
--- src/SharpAudio.Tests/Engines.cs
+++ src/SharpAudio.Tests/Engines.cs
@@ -3,38 +3,44 @@ using Xunit;
 
 namespace SharpAudio.Tests
 {
-    public class Engines
+  public class Engines
+  {
+    [Fact]
+    public void CreateXAudio()
     {
-        [Fact]
-        public void CreateXAudio()
-        {
-            TestEngine(AudioEngine.CreateXAudio());  
-        }
-
-        [Fact]
-        public void CreateOpenAL()
-        {
-            TestEngine(AudioEngine.CreateOpenAL());
-        }
-
-        void TestEngine(AudioEngine engine)
-        {
-            var buffer = engine.CreateBuffer();
-            var source = engine.CreateSource();
-
-            AudioFormat format;
-            format.BitsPerSample = 16;
-            format.Channels = 1;
-            format.SampleRate = 8000;
-
-            short[] data = { 0, -100, 100, -200, 200 };
-
-            buffer.BufferData(data, format);
-
-            source.QueryBuffer(buffer);
-
-            source.Play();
-
-        }
+      TestEngine(AudioEngine.CreateXAudio());
     }
+
+    [Fact]
+    public void CreateOpenAL()
+    {
+      TestEngine(AudioEngine.CreateOpenAL());
+    }
+
+    void TestEngine(AudioEngine engine)
+    {
+      var buffer = engine.CreateBuffer();
+      var source = engine.CreateSource();
+
+      AudioFormat format;
+      format.BitsPerSample = 16;
+      format.Channels = 1;
+      format.SampleRate = 44100;
+      float freq = 440.0f;
+      var size = format.SampleRate;
+      var samples = new short[size];
+
+      for (int i = 0; i < size; i++)
+      {
+        samples[i] = (short)(32760 * Math.Sin((2 * Math.PI * freq) / size * i));
+      }
+
+      buffer.BufferData(samples, format);
+
+      source.QueryBuffer(buffer);
+
+      source.Play();
+
+    }
+  }
 }
